c48e9591c5b2497c1a6c9fb1f5e3a5798a88f7fa
Stuart Thompson <stuart@stuartthompson.net>
2019-07-23 12:30:34 -0700

    Building a vec2d class.

---
+++ Angle.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/**
+ * Converts degrees to radians.
+ */
+const float degreesToRadians(const float degrees);
+
+/**
+ * Converts degrees to radians.
+ */
+const double degreesToRadians(const double degrees);
+
+/**
+ * Converts radians to degrees.
+ */
+const float radiansToDegrees(const float radians);
+
+/**
+ * Converts radians to degrees.
+ */
+const double radiansToDegrees(const double radians);
