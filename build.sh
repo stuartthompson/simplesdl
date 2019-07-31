adbcad995f96dfc9157e05a23c4e6b0b18074aac
Stuart Thompson <stuart@stuartthompson.net>
2019-07-30 22:05:50 -0700

    Multiple build targets.

--- build.sh
+++ build.sh
@@ -16,29 +16,34 @@ OUTPUT=sdltest
 CFLAGS="-std=c++17"
 
 # Set platform-specific variables
-if [ "$1" = "linux" ]; then
-    echo "Building for Linux"
+if [ "$PLATFORM" == "linux" ]; then
+    echo "Targeting Linux"
     PLATFORM="linux"
     INC="-I/usr/include/zenixel -I/usr/include/SDL2"
     LIB="-L/usr/lib -lSDL2 -lSDL2_ttf -lzenixel"
     CFLAGS="-D_REENTRANT $CFLAGS"
 fi
-if [ "$1" = "osx" ]; then
-    echo "Building for OSX"
+if [ "$PLATFORM" == "osx" ]; then
+    echo "Targeting OSX"
     PLATFORM="osx"
     INC="-I/usr/local/include/zenixel -I/usr/local/include/SDL2"
     LIB="-L/usr/local/lib -lSDL2 -lSDL2_ttf -lzenixel"
     CFLAGS="-D_THREAD_SAFE $CFLAGS"
 fi
 
-# Clean up previous builds
-echo "Cleaning build output"
-rm -rf build                                                # Remove build folder
 
-# Create output folder
-echo "Creating build folder"
-mkdir $BUILDDIR                                             # Create build folder
+if [ "$TASK" == "clean" ] || [ "$TASK" == "" ]; then
+    # Clean up previous builds
+    echo "Cleaning build output"
+    rm -rf $BUILDDIR                                             # Remove build folder
+fi
 
-# Compile object files
-echo "Compiling program"
-g++ -g $SRCROOT/main.cpp $INC $LIB -o $BUILDDIR/$OUTPUT $CFLAGS
+if [ "$TASK" == "build" ] || [ "$TASK" == "" ]; then
+    # Create output folder
+    echo "Creating build folder"
+    mkdir $BUILDDIR                                             # Create build folder
+
+    # Compile program
+    echo "Compiling program"
+    g++ -g $SRCROOT/main.cpp $INC $LIB -o $BUILDDIR/$OUTPUT $CFLAGS
+fi
